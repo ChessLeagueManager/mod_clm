a57469f5c79f7602498c9983ee5367885bf74601
Klaus Bärthel <kbaerthel@t-online.de>
2024-09-14 13:41:12 +0200

    Version 4.2.0 + neue Parameter Kategorie und Region

--- helper.php
+++ helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @ Chess League Manager (CLM) Component 
- * @Copyright (C) 2008-2020 CLM Team.  All rights reserved
+ * @Copyright (C) 2008-2024 CLM Team.  All rights reserved
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  * @link http://www.chessleaguemanager.de
 */
@@ -20,7 +20,7 @@ class modCLMHelper {
 			// String zerlegen
 			$array_ids = explode(",", $par_ids);
 			if (count($array_ids) > 0) {
-				$sqlIDs = " AND (";
+				$sqlIDs = " (";
 				$counter = 0; // zählt tatsächliche abgefragte IDs
 				foreach ($array_ids as $value) {
 					settype($value, "int");
@@ -42,6 +42,55 @@ class modCLMHelper {
 		} else {
 			$sqlIDs = "";
 		}
+		// Kategorie gegeben?
+		$par_cat = $params->def('catid', '');
+		if ( $par_cat == 0 ) $par_cat = '';
+		if ($par_cat != '') {
+			// Teil der where-Klausel
+			$sqlcat = " ( catidAllTime = $par_cat OR catidEdition = $par_cat ) ";
+		} else {
+			// falls doch keine Kategorie eingetragen wurde
+			$sqlcat = "";
+		}
+		// Region gegeben?
+		$par_verbd = $params->def('verbd', '');
+		if ($par_verbd != '' AND (substr($par_verbd, -1, 1) == '0')) {
+			$par_verbd = substr($par_verbd, 0, strlen($par_verbd)-1);
+			if ($par_verbd != '' AND (substr($par_verbd, -1, 1) == '0')) {
+				$par_verbd = substr($par_verbd, 0, strlen($par_verbd)-1);
+				if ($par_verbd != '' AND (substr($par_verbd, -1, 1) == '0')) {
+					$par_verbd = substr($par_verbd, 0, strlen($par_verbd)-1);
+				}
+			}
+		}
+		if ($par_verbd != '') {
+			$l_verbd = strlen($par_verbd);
+//			$query = "SELECT  a.sid, a.liga, a.name, a.zps, SUBSTRING(a.zps, 1, $l_verbd) as tzps "
+			$query = "SELECT  a.liga "
+				."\n FROM #__clm_mannschaften as a"
+				."\n LEFT JOIN #__clm_saison as s ON s.id = a.sid "
+				."\n WHERE a.published = 1"
+				."\n AND s.published = 1"
+				."\n AND SUBSTRING(a.zps, 1, $l_verbd) = '$par_verbd' "
+				;
+			if ($par_saison == 0)	
+				$query .= "\n AND s.archiv  != 1";
+			else
+				$query .= "\n AND s.id  = ".$par_saison;
+			$query .= "\n GROUP BY a.liga ";
+				$db->setQuery( $query );
+				$ligen = $db->loadObjectList();
+				$a_ligen = array();
+				foreach ($ligen as $lid) {
+					$a_ligen[] = $lid->liga;;
+				}
+				$lids = implode(',', $a_ligen);
+			// Teil der where-Klausel
+			$sqlverbd = " ( FIND_IN_SET(a.id,'".$lids."') != 0 ) ";
+		} else {
+			// falls doch keine Region eingetragen wurde
+			$sqlverbd = "";
+		}
 		
 	
 		$query = "SELECT  a.sid, a.id, a.name, a.runden, a.durchgang, a.rang, a.runden_modus, a.liga_mt, a.params "
@@ -51,9 +100,18 @@ class modCLMHelper {
 			.($par_mt_type < 2 ? "\n AND a.liga_mt = ".$par_mt_type : "")
 			."\n AND s.published = 1";
 		if ($par_saison == 0)	
-			$query .= "\n AND s.archiv  != 1".$sqlIDs;
+			$query .= "\n AND s.archiv  != 1";
 		else
-			$query .= "\n AND s.id  = ".$par_saison.$sqlIDs;
+			$query .= "\n AND s.id  = ".$par_saison;
+		if ($sqlcat != '' OR $sqlIDs != ''  OR $sqlverbd != '') {		
+			if ($sqlcat != '' AND $sqlIDs != '' AND $sqlverbd != '') $query .= "\n AND ( $sqlcat OR $sqlIDs OR $sqlverbd)";
+			elseif ($sqlcat != '' AND $sqlIDs != '') $query .= "\n AND ( $sqlcat OR $sqlIDs )";
+			elseif ($sqlcat != '' AND $sqlverbd != '') $query .= "\n AND ( $sqlcat OR $sqlverbd )";
+			elseif ($sqlIDs != '' AND $sqlverbd != '') $query .= "\n AND ( $sqlIDs OR $sqlverbd )";
+			elseif ($sqlcat != '') $query .= "\n AND ( $sqlcat )";
+			elseif ($sqlIDs != '') $query .= "\n AND ( $sqlIDs )";
+			elseif ($sqlverbd != '') $query .= "\n AND ( $sqlverbd )";
+		}
 		$query .= "\n ORDER BY a.sid DESC,a.ordering ASC, a.id ASC "
 			;
 		$db->setQuery( $query );
